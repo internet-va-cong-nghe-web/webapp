0f48ba28db792c7013056c25d2712f7d1e9b076d
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2024-10-09 20:40:07 +0700

    config axios to server, front end UI signup sign in for app.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isSignUp, setIsSignUp] = useState(true); // true for sign-up, false for sign-in
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (isSignUp) {
-        // Gửi yêu cầu đăng ký
-        const response = await axios.post('http://localhost:5000/api/signup', {
-          username,
-          password,
-        });
-        alert(response.data.message);
-      } else {
-        // Gửi yêu cầu đăng nhập
-        const response = await axios.post('http://localhost:5000/api/signin', {
-          username,
-          password,
-        });
-        alert(response.data.message);
-        console.log('Token:', response.data.token);
-      }
-    } catch (error) {
-      alert(error.response.data.message);
-    }
-  };
+	const user = localStorage.getItem("token");
 
-  return (
-    <div>
-      <p>HELLO WORLD</p>
-      <h1>{isSignUp ? 'Sign Up' : 'Sign In'}</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">{isSignUp ? 'Sign Up' : 'Sign In'}</button>
-      </form>
-      <button onClick={() => setIsSignUp(!isSignUp)}>
-        Switch to {isSignUp ? 'Sign In' : 'Sign Up'}
-      </button>
-    </div>
-  );
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
