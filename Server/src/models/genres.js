b8a5e5f8da4a1319cdde3b814482519fb0dad736
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2024-12-22 08:45:16 +0700

    genres schema

---
+++ Server/src/models/genres.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const GenreSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Genre", GenreSchema);
